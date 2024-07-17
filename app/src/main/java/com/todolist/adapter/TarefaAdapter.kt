b8d38c01947c18ae9be6174d6eaa9c418cc5a316
Matheus <matheus.pino@hotmail.com>
2024-07-17 19:22:58 -0300

    Ajuste de classe Adapter e recyclerView

--- app/src/main/java/com/todolist/adapter/TarefaAdapter.kt
+++ app/src/main/java/com/todolist/adapter/TarefaAdapter.kt
@@ -1,12 +1,20 @@
 package com.todolist.adapter
 
 import android.content.Context
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.todolist.R
 import com.todolist.model.Tarefa
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
+import java.util.concurrent.TimeUnit
 
-class TarefaAdapter(private val context: Context, private val tarefas: List<Tarefa> ): RecyclerView.Adapter<TarefaAdapter.TarefaViewHolder>() {
+class TarefaAdapter(private val context: Context, private val tarefas: List<Tarefa>) :
+    RecyclerView.Adapter<TarefaAdapter.TarefaViewHolder>() {
     override fun equals(other: Any?): Boolean {
         return super.equals(other)
     }
@@ -14,12 +22,36 @@ class TarefaAdapter(private val context: Context, private val tarefas: List<Tare
     override fun onCreateViewHolder(
         parent: ViewGroup,
         viewType: Int
-    ): TarefaAdapter.TarefaViewHolder {
-        TODO("Not yet implemented")
+    ): TarefaViewHolder {
+        val view = LayoutInflater.from(context).inflate(R.layout.item_tarefa, parent, false)
+        return TarefaViewHolder(view)
     }
 
-    override fun onBindViewHolder(holder: TarefaAdapter.TarefaViewHolder, position: Int) {
-        TODO("Not yet implemented")
+    override fun onBindViewHolder(holder: TarefaViewHolder, position: Int) {
+        val tarefa = tarefas[position]
+        holder.tituloTextView.text = tarefa.titulo
+
+        val diasRestantes = calcularDiasRestantes(tarefa.data)
+        holder.diasRestantesTextView.text = "$diasRestantes"
+
+        val backgroundResource = if (Date().after(converterStringParaData(tarefa.data))) {
+            R.drawable.border_red
+        } else {
+            R.drawable.border_green
+        }
+        holder.itemView.setBackgroundResource(backgroundResource)
+    }
+
+    private fun calcularDiasRestantes(data: String): Long {
+        val dateFormat = SimpleDateFormat("dd/MM/yyyy", Locale.getDefault())
+        val data: Date = dateFormat.parse(data)
+        val diferenca = data.time - Date().time
+        return TimeUnit.DAYS.convert(diferenca, TimeUnit.MILLISECONDS)
+    }
+
+    private fun converterStringParaData(data: String): Date {
+        val dateFormat = SimpleDateFormat("dd/MM/yyyy", Locale.getDefault())
+        return dateFormat.parse(data)
     }
 
     override fun onBindViewHolder(
@@ -34,8 +66,9 @@ class TarefaAdapter(private val context: Context, private val tarefas: List<Tare
         TODO("Not yet implemented")
     }
 
-    class TarefaViewHolder(itemView: View): RecyclerView.ViewHolder(itemView){
-
+    class TarefaViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val tituloTextView: TextView = itemView.findViewById(R.id.tituloTextView)
+        val diasRestantesTextView: TextView = itemView.findViewById(R.id.diasRestantesTextView)
     }
 
 }
--- app/src/main/java/com/todolist/view/ListarFragment.kt
+++ app/src/main/java/com/todolist/view/ListarFragment.kt
@@ -1,15 +1,40 @@
 package com.todolist.view
 
+import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.LinearLayout
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.todolist.R
+import com.todolist.adapter.TarefaAdapter
+import com.todolist.repository.IRepository
+import com.todolist.repository.RepositoryImpl
 
 class ListarFragment : Fragment() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+
+    private lateinit var repository: IRepository
+    private lateinit var adapter: TarefaAdapter
+    private lateinit var recyclerView: RecyclerView
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        repository = RepositoryImpl(
+            sharedPreference = requireContext().getSharedPreferences(
+                "lista_tarefas",
+                Context.MODE_PRIVATE
+            )
+        )
+        recyclerView = view.findViewById(R.id.recyclerView)
+        recyclerView.layoutManager = LinearLayoutManager(context)
+
+        val tarefas = repository.obterTodasTarefas()
+
+        adapter = TarefaAdapter(requireContext(), tarefas)
+
     }
 
     override fun onCreateView(
@@ -19,6 +44,5 @@ class ListarFragment : Fragment() {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_listar, container, false)
     }
-
 
 }
